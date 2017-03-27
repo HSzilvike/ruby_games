9706b020e002e3eb7be4af1eebb679bb5f44045e
Szilvia Harmat <szh.nz@yahoo.com>
2017-03-27 16:59:19 +1300

    User can guess computer choice

---
+++ mastermind1.rb
@@ -0,0 +1,76 @@
+colors = ["red", "blue", "green", "yellow", "black", "white"]
+$choice = colors.sample(4)
+
+def make_guess
+  
+  guess = Array.new
+  puts "Make a guess! Type in 4 colors. The choices: red, blue, green, yellow, black or white"
+  
+  i=0
+ while (guess.length < 4) do
+  somestring = gets.chomp.downcase
+  if ["red", "blue", "green", "yellow", "black", "white"].include?(somestring)
+     guess[i] = somestring
+     i+=1
+   else
+     puts "Please choose from: red, blue, green, yellow, black or white"
+    end
+  end
+  
+  puts "Your guess is #{guess}."
+  
+  $guess = guess
+end
+
+def feedback(choice, guess)
+  
+  matching_colors = 0
+  matching_position = 0
+  
+  
+  if choice == guess
+    puts "Congratulations! You win!"
+    return $gameover = TRUE
+  else
+    $gameover = FALSE
+  end
+  
+  i=0
+  4.times do |i|
+    if (guess[i] == choice[i])
+      matching_position +=1
+    end
+    
+    if choice.include?(guess[i])
+    matching_colors+=1
+    end
+  
+    i+=1
+  end
+  
+  puts "Number of matching colors: #{matching_colors}."
+  puts "Number of matching positions: #{matching_position}."
+  #puts "For debugging: the choice is #{choice}."
+end
+
+
+
+
+def game
+  
+12.times do
+  make_guess
+  feedback($choice, $guess)
+  break if $gameover
+  end
+  
+puts "You lose! The solution is #{$choice}. Try again!"
+  
+end
+
+
+game
+
+
+
+
