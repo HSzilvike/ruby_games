3ab0b73a95cfb899de13b7f89b407e5d01f2c625
Szilvia Harmat <szh.nz@yahoo.com>
2017-04-11 00:32:32 +1200

    Human can choose codemaker, computer guess random

--- mastermind2.rb
+++ mastermind2.rb
@@ -11,6 +11,37 @@ class Player
   def input_correct?(input)
     COLORS.include?(input)
   end
+  
+  def feedback_input_correct?(input)
+    ["0","1","2"].include?(input)
+  end
+  
+  def human_feedback_correct?(guess, code, feedback)
+    correct_fb = correct_feedback(guess, code)
+    correct = true
+    feedback.each_with_index do |item, index|
+      if (item == correct_fb[index])
+        next
+      else
+        correct = false
+      end
+    end
+    return correct
+  end
+  
+  def correct_feedback(guess, code)
+    feedback = []
+    guess.each_with_index do |item, index|
+      if item == code[index]
+        feedback.push("2")
+      elsif code.include?(item)
+        feedback.push("1")
+      else
+        feedback.push("0")
+      end
+    end
+    feedback
+  end
 end
 
 class HumanPlayer < Player
@@ -28,9 +59,22 @@ class HumanPlayer < Player
     enter_colors
   end
   
-  def create_code #ez még nem kell
+  def create_code
     enter_colors
   end
+  
+  def give_feedback(guess, code)
+    puts "Please enter 2 if color & position are right; 1 if the color is right ; 0 if the color is not included in the secret code, for each guess, one at a time."
+    puts "Reminder: your secret code is #{code}."
+    feedback = []
+    while feedback.length < 4 do
+      input = gets.chomp
+      feedback_input_correct?(input) ? feedback.push(input) : (puts "Please enter 0, 1 or 2"; next)
+      human_feedback_correct?(guess, code, feedback) ? next : (puts "This feedback seems to be incorrect. Check again!" ; feedback.pop)
+    end
+    feedback
+  end
+
 end
 
 class ComputerPlayer < Player
@@ -38,30 +82,37 @@ class ComputerPlayer < Player
     COLORS.sample(4)
   end
   
+  def make_guess
+    @board.feedback.empty? ? random_guess(4) : evaluate_feedback
+  end
+  
+  def evaluate_feedback
+    newguess = []
+    (0..3).each do |i|
+    (@board.feedback[i] == "2") ? newguess.push(@board.guess[i]) : newguess.push(random_guess(1).join)
+    end
+    newguess
+  end
+  
+  def random_guess(n)
+    COLORS.sample(n)
+  end
   
   def give_feedback(guess, code)
-    feedback = []
-    guess.each_with_index do |item, index|
-      if item == code[index]
-        feedback.push("2")
-      elsif code.include?(item)
-        feedback.push("1")
-      else
-        feedback.push("0")
-      end
-    end
-    feedback
+    correct_feedback(guess, code)
   end
+  
 end
 
 
 class Board
-  
-  def initialize(name_human, name_computer)
-    @codemaker = ComputerPlayer.new(name_computer, self)
-    @codebreaker = HumanPlayer.new(name_human, self)
+  attr_reader :feedback, :guess
+  def initialize(breaker_class, breaker_name, maker_class, maker_name)
+    @codemaker = maker_class.new(maker_name, self)
+    @codebreaker = breaker_class.new(breaker_name, self)
     @secretcode = []
     @guess = []
+    @feedback = []
     @num_round = 1
   end
   
@@ -112,7 +163,22 @@ class Board
   end
 end
 
-Board.new("Muci", "MastermindBot").play
+
+puts "Welcome to Mastermind! Please enter your name:"
+username = gets.chomp
+puts "Do you want to make or break the secret code? Enter M or B"
+game_type = gets.chomp.to_s.upcase
+
+  case game_type
+  when "M"
+    Board.new(ComputerPlayer, "MastermindBot", HumanPlayer, username).play
+  when "B"
+    Board.new(HumanPlayer, username, ComputerPlayer, "MastermindBot").play
+  else
+    puts "Please enter M or B"
+  end
+
+
 
 
 
